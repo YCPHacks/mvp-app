5291b06a141cf726e7513be1fac2f26095504747
pwalter1014 <pwalter3@ycp.edu>
2023-11-12 15:07:16 -0500

    updated implementation of endpoint
    hardware_rentals_current now takes the page number, page size, search and categories from URL and applies it to stored procedure to return a list of items

--- routes/hardware_rentals_current/index.js
+++ routes/hardware_rentals_current/index.js
@@ -4,7 +4,27 @@ const mysqlx = require('@mysql/xdevapi');
 module.exports = async function (fastify, options) {
     fastify.get('/', async function (request, reply) {
         const session = await mysqlx.getSession(process.env.MYSQLX_HARDWARE_DATABASE_URL);
-        const statement = "CALL select_hardware_items_and_current_record";
+        const search = request.query.search ?? '';
+        //console.log(search);
+        const categories = request.query.categories ?? '[]';
+        //console.log(categories);
+        const pageSize = request.query.pageSize ?? 20;
+        //console.log(pageSize);
+        const pageNumber = request.query.pageNumber ?? 1;
+        //console.log(pageNumber);
+        await session.sql('SET @search = ?;')
+                        .bind(search)
+                        .execute();
+        await session.sql('SET @categories = ?;')
+                        .bind(categories)
+                        .execute();
+        await session.sql('SET @pageSize = ?;')
+                        .bind(pageSize)
+                        .execute();
+        await session.sql('SET @pageNumber = ?;')
+                        .bind(pageNumber)
+                        .execute();
+        const statement = "CALL list_all_hardware_items_and_current_record(@search, @categories, @pageSize, @pageNumber)";
         const result = await session.sql(statement).execute();
         const rent_current = await result.fetchAll();
         const data = rent_current.map((item) => {
@@ -25,6 +45,24 @@ module.exports = async function (fastify, options) {
             "length": rent_current.length,
             "data": data
         }
+
+        return scheme;
+    });
+
+    fastify.get('/:id', async function (request, reply) {
+        const session = await mysqlx.getSession(process.env.MYSQLX_HARDWARE_DATABASE_URL);
+        await session.sql('SET @id = ?;')
+                        .bind(request.params.id)
+                        .execute();
+        const statement = "CALL list_current_rental_by_hardware_id(@id)";
+        const result = await session.sql(statement).execute();
+        const columns = await result.getColumns();
+        const current = await result.fetchOne();
+        const scheme = current.reduce((res, value, i) => {
+            return Object.assign({}, res, { [columns[i].getColumnLabel()]: value })
+          }, {});
+
+        await session.close();
 
         return scheme;
     });
