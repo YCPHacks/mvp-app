a2e1de6ae5a613030550e361acc8d5ce4e78b9ae
pwalter1014 <pwalter3@ycp.edu>
2023-11-26 12:13:48 -0500

    updated mvp api. added more handler functions

--- routes/hardware_inventory/routes.js
+++ routes/hardware_inventory/routes.js
@@ -19,30 +19,51 @@ module.exports = async function hardwareRoutes(fastify, options) {
     fastify.route({
         method: 'GET',
         url: '/',
-        // schema: {
-        //     querystring: fastify.getSchema('schema:hardware:list:query'),
-        //     response: {
-        //         200: fastify.getSchema('schema:hardware:item:response')
-        //     }
-        // },
         preValidation: fastify.authenticate,
         handler: listHardware
     })
 
+    async function createHardware(request, reply){
+        //get the label for the body of the HTTP request
+        const label = request.body.label;
+        //name of stored procedure to be called
+        const statement = 'CALL create_hardware_inventory_item(?);';
+        //establish connection to database
+        const connection = await fastify.mysql.getConnection()
+        //execute prepared statement using label requested from HTTP request body
+        connection.execute(statement, [label]);
+        //release connection from database
+        connection.release()
+
+        reply.code(201)
+    }
+
     fastify.route({
         method: 'POST',
         url: '/',
-        handler: async function createHardware(request, reply) {
-            return { id: '123' }
-        }
+        handler: createHardware
     })
+
+    async function readHardware(request, reply) {
+        //get the id for the HTTP request
+        const id = request.params.id
+        //name of stored procedure to be called
+        const statement = 'CALL read_hardware_inventory_item(?);'
+        //establish connection to database
+        const connection = await fastify.mysql.getConnection()
+        //execute prepared statement using id requested from HTTP request
+        const [rows] = await connection.execute(statement, [id])
+        const data = rows[0]
+        //release connection from database
+        connection.release()
+
+        return {data}
+    }
 
     fastify.route({
         method: 'GET',
         url: '/:id',
-        handler: async function readHardware(request, reply) {
-            return {}
-        }
+        handler: readHardware
     })
 
     fastify.route({
@@ -52,6 +73,22 @@ module.exports = async function hardwareRoutes(fastify, options) {
             reply.code(204)
         }
     })
+
+    async function deleteHardware(request, reply) {
+        //get the id for the HTTP request
+        const id = request.params.id
+        const time_of_deletion = new Date()
+        //name of stored procedure to be called
+        const statement = 'CALL delete_hardware_inventory_item(?, ?);'
+        //establish connection to database
+        const connection = await fastify.mysql.getConnection()
+        //execute prepared statement using id requested from HTTP request
+        connection.execute(statement, [id, time_of_deletion])
+        //release connection from database
+        connection.release()
+
+        reply.code(204)
+    }
 
     fastify.route({
         method: 'DELETE',
